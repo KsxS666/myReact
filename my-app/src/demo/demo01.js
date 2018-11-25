dea802c188b0d59be469ba3e7791731ba89b7108
WIN-IK21D8CKFKE\Administrator <693939485@qq.com>
2018-11-25 18:06:20 +0800

    我的reactTest项目第一次推送

---
+++ my-app/src/demo/demo01.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class HelloComponent extends Component {
+    // render() {
+    //     return React.createElement('h1', {className:"hello"}, 'Hello World');
+    // }
+    render(){
+        return <h1>Hello{this.props.name?this.props.name:"World"}</h1>
+    }
+}
+export default HelloComponent
